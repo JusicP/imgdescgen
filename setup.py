7630fdbe8687947fd1562fb1f364b1ff1cfb291f
JusicP <slender87844@gmail.com>
2025-05-09 20:45:43 +0300

    Use find_packages instead of hardcoded packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='imgdescgenlib',
     version='1.0',
-    packages=['imgdescgenlib', 'imgdescgenlib/chatbot'],
+    packages=find_packages(),
     install_requires=[
         'pyexiftool>=0.5.6', 'requests>=2.32.3', 'pillow>=11.1.0', 'pytest>=8.3.5', 'pydantic-settings>=2.8.1',
     ],
